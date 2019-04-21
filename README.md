acde0d06945be9c3f91f2ffe1828972549b3c8ec
Sakamoto, Kazunori <exkazuu@gmail.com>
2019-04-21 11:14:28 +0900

    feat: add initial files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# PlantUML-Visualizer
+# PlantUML Visualizer
+
---
+++ src/main.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div> Your App injected to DOM correctly! </div>
+    )
+  }
+}
+
+// Message Listener function
+chrome.runtime.onMessage.addListener((request, sender, response) => {
+  // If message is injectApp
+  if(request.injectApp) {
+    // Inject our app to DOM and send response
+    injectApp();
+    response({
+      startedExtension: true,
+    });
+  }
+});
+
+function injectApp() {
+  const newDiv = document.createElement("div");
+  newDiv.setAttribute("id", "chromeExtensionReactApp");
+  document.body.appendChild(newDiv);
+  ReactDOM.render(<App />, newDiv);
+}
