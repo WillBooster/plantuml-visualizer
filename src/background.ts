40358152ff530e9614f0a216680d79a7caeded0c
kumachan-mis <y.s11nov@fuji.waseda.jp>
2019-07-04 10:57:41 +0900

    add gray icon

--- src/background.ts
+++ src/background.ts
@@ -36,7 +36,7 @@ chrome.browserAction.setIcon({ path: 'icon/icon16.png' });
 
 chrome.browserAction.onClicked.addListener(tab => {
   extensionEnabled = !extensionEnabled;
-  chrome.browserAction.setIcon({ path: extensionEnabled ? 'icon/icon16.png' : 'icon/icon16mono.png' });
+  chrome.browserAction.setIcon({ path: extensionEnabled ? 'icon/icon16.png' : 'icon/icon16gray.png' });
   if (tab.id) chrome.tabs.reload(tab.id);
 });
 
