015f7481e7580c97d2697cc1d78aa53583bdc6aa
Sakamoto, Kazunori <exkazuu@gmail.com>
2019-12-10 00:12:30 +0900

    chore: willboosterify this repo upgrading dependencies

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,9 @@
+const micromatch = require('micromatch');
 module.exports = {
-  './**/*.{css,htm,html,js,json,jsx,md,scss,ts,tsx,vue,yaml,yml}': files => {
-    const filtered = files.filter(file => !file.includes('/test-fixtures/')).join(' ');
-    return filtered ? [`prettier --write ${filtered}`, `git add ${filtered}`] : [];
-  },
   './{packages/*/,}{src,__tests__}/**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'git add'],
+  './**/*.{css,htm,html,js,json,jsx,md,scss,ts,tsx,vue,yaml,yml}': files => {
+    files = micromatch.not(files, './{packages/*/,}{src,__tests__}/**/*.{js,jsx,ts,tsx}');
+    const fileList = files.filter(file => !file.includes('/test-fixtures/')).join(' ');
+    return fileList ? [`prettier --write ${fileList}`, `git add ${fileList}`] : [];
+  },
 };
