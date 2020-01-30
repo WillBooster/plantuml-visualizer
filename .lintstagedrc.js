0ea57d284f016de1054994e8cb7607a1da1f85ee
Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>
2020-01-30 19:08:51 +0900

    chore: willboosterify this repo upgrading dependencies (#390)

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,16 @@
 const micromatch = require('micromatch');
+const path = require('path');
+
 module.exports = {
-  './{packages/*/,}{src,__tests__}/**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'git add'],
+  './{packages/*/,}{src,__tests__}/**/*.{js,jsx,ts,tsx}': ['eslint --fix'],
   './**/*.{css,htm,html,js,json,jsx,md,scss,ts,tsx,vue,yaml,yml}': files => {
     files = micromatch.not(files, './{packages/*/,}{src,__tests__}/**/*.{js,jsx,ts,tsx}');
-    const fileList = files.filter(file => !file.includes('/test-fixtures/')).join(' ');
-    return fileList ? [`prettier --write ${fileList}`, `git add ${fileList}`] : [];
+    const filteredFiles = files.filter(file => !file.includes('/test-fixtures/')).map(file => path.relative('', file));
+    if (filteredFiles.length === 0) return [];
+    const commands = [`prettier --write ${filteredFiles.join(' ')}`];
+    if (filteredFiles.some(file => file.endsWith('package.json'))) {
+      commands.push('yarn sort-package-json');
+    }
+    return commands;
   },
 };
