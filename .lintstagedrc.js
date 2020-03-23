02d57204209faff1b910e0a6024699cbf4ea002d
Sakamoto, Kazunori <exkazuu@gmail.com>
2020-03-23 20:40:08 +0900

    chore: willboosterify this repo upgrading Prettier

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,12 +3,14 @@ const path = require('path');
 
 module.exports = {
   './{packages/*/,}{src,__tests__}/**/*.{js,jsx,ts,tsx}': ['eslint --fix'],
-  './**/*.{css,htm,html,js,json,jsx,md,scss,ts,tsx,vue,yaml,yml}': files => {
+  './**/*.{css,htm,html,js,json,jsx,md,scss,ts,tsx,vue,yaml,yml}': (files) => {
     files = micromatch.not(files, './{packages/*/,}{src,__tests__}/**/*.{js,jsx,ts,tsx}');
-    const filteredFiles = files.filter(file => !file.includes('/test-fixtures/')).map(file => path.relative('', file));
+    const filteredFiles = files
+      .filter((file) => !file.includes('/test-fixtures/'))
+      .map((file) => path.relative('', file));
     if (filteredFiles.length === 0) return [];
     const commands = [`prettier --write ${filteredFiles.join(' ')}`];
-    if (filteredFiles.some(file => file.endsWith('package.json'))) {
+    if (filteredFiles.some((file) => file.endsWith('package.json'))) {
       commands.push('yarn sort-package-json');
     }
     return commands;
