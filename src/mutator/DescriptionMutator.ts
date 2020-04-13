a8082146a7ce4bc2fbbf58147401c30fee3533f7
kumachan-mis <y.s11nov@fuji.waseda.jp>
2020-04-13 19:48:30 +0900

    refine based on review

--- src/mutator/DescriptionMutator.ts
+++ src/mutator/DescriptionMutator.ts
@@ -3,27 +3,31 @@ import { markAsAlreadyProcessed, textToImage } from './MutatorUtil';
 
 export const DescriptionMutator = {
   async embedPlantUmlImages(finders: Finder[], webPageUrl: string, $root: JQuery<Node>) {
-    const arraysOfContents = await Promise.all(finders.map((finder) => finder.find(webPageUrl, $root)));
-    for (const content of arraysOfContents.flat()) {
-      // Skip if no PlantUML descriptions exist
-      if (!content.text.length) continue;
+    await Promise.all(
+      finders.map(async (finder) => {
+        const contents = await finder.find(webPageUrl, $root);
+        for (const content of contents) {
+          // Skip if no PlantUML descriptions exist
+          if (!content.text.length) continue;
 
-      const $text = content.$text;
+          const $text = content.$text;
 
-      // To avoid embedding an image multiple times
-      if (!markAsAlreadyProcessed($text)) continue;
-      const $image = await textToImage(content.text);
-      $image.insertAfter($text);
+          // To avoid embedding an image multiple times
+          if (!markAsAlreadyProcessed($text)) continue;
+          const $image = await textToImage(content.text);
+          $image.insertAfter($text);
 
-      $text.on('dblclick', () => {
-        $text.hide();
-        $image.show();
-      });
-      $image.on('dblclick', () => {
-        $image.hide();
-        $text.show();
-      });
-      $text.dblclick();
-    }
+          $text.on('dblclick', () => {
+            $text.hide();
+            $image.show();
+          });
+          $image.on('dblclick', () => {
+            $image.hide();
+            $text.show();
+          });
+          $text.dblclick();
+        }
+      })
+    );
   },
 };
