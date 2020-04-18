435e0760353a745fdc4c93e71a76a3f267190947
Sakamoto, Kazunori <exkazuu@gmail.com>
2020-04-18 13:30:12 +0900

    Support DiffMutator

--- src/mutator/diffMutator.ts
+++ src/mutator/diffMutator.ts
@@ -14,64 +14,61 @@ export const DiffMutator = {
           const $diff = content.$diff;
 
           // To avoid embedding an image multiple times
-          if (!markAsAlreadyProcessed($diff)) continue;
-          const [baseImages, headImages] = await Promise.all([
-            textsToImages(content.baseTexts, 'Nothing'),
-            textsToImages(content.headTexts, 'Deleted'),
-          ]);
+          let $visualizedDiff: JQuery<Node>;
+          if (markAsAlreadyProcessed($diff)) {
+            $visualizedDiff = $(`<div></div>`);
 
-          for (const $image of baseImages) {
-            $image.css('background-color', '#ffeef0');
+            const $baseBranchMark = $(`<div>${content.baseBranchName}</div>`);
+            const $headBranchMark = $(`<div>${content.headBranchName}</div>`);
+
+            $baseBranchMark
+              .css('padding', '4px 10px')
+              .css('background-color', '#ffdce0')
+              .css('color', 'rgba(27,31,35,.7)')
+              .css('font-size', '12px');
+
+            $headBranchMark
+              .css('padding', '4px 10px')
+              .css('background-color', '#cdffd8')
+              .css('color', 'rgba(27,31,35,.7)')
+              .css('font-size', '12px');
+
+            const [baseImages, headImages] = await Promise.all([
+              textsToImages(content.baseTexts, 'Nothing'),
+              textsToImages(content.headTexts, 'Deleted'),
+            ]);
+
+            for (const $image of baseImages) {
+              $image.css('background-color', '#ffeef0');
+            }
+            for (const $image of headImages) {
+              $image.css('background-color', '#e6ffed');
+            }
+
+            $visualizedDiff.append($baseBranchMark);
+            baseImages[0].insertAfter($baseBranchMark);
+            for (let i = 1; i < baseImages.length; i++) {
+              baseImages[i].insertAfter(baseImages[i - 1]);
+            }
+            $headBranchMark.insertAfter(baseImages[baseImages.length - 1]);
+            headImages[0].insertAfter($headBranchMark);
+            for (let i = 1; i < headImages.length; i++) {
+              headImages[i].insertAfter(headImages[i - 1]);
+            }
+
+            $visualizedDiff.insertAfter($diff);
+          } else {
+            $visualizedDiff = $diff.next();
           }
-          for (const $image of headImages) {
-            $image.css('background-color', '#e6ffed');
-          }
 
-          const $baseBranchMark = $(`<div>${content.baseBranchName}</div>`);
-          const $headBranchMark = $(`<div>${content.headBranchName}</div>`);
-
-          $baseBranchMark
-            .css('padding', '4px 10px')
-            .css('background-color', '#ffdce0')
-            .css('color', 'rgba(27,31,35,.7)')
-            .css('font-size', '12px');
-
-          $headBranchMark
-            .css('padding', '4px 10px')
-            .css('background-color', '#cdffd8')
-            .css('color', 'rgba(27,31,35,.7)')
-            .css('font-size', '12px');
-
-          $baseBranchMark.insertAfter($diff);
-          baseImages[0].insertAfter($baseBranchMark);
-          for (let i = 1; i < baseImages.length; i++) {
-            baseImages[i].insertAfter(baseImages[i - 1]);
-          }
-          $headBranchMark.insertAfter(baseImages[baseImages.length - 1]);
-          headImages[0].insertAfter($headBranchMark);
-          for (let i = 1; i < headImages.length; i++) {
-            headImages[i].insertAfter(headImages[i - 1]);
-          }
-
-          const images = baseImages.concat(headImages);
           $diff.on('dblclick', () => {
             $diff.hide();
-            for (const $image of images) {
-              $image.show();
-            }
-            $baseBranchMark.show();
-            $headBranchMark.show();
+            $visualizedDiff.show();
           });
-          for (const $image of images) {
-            $image.on('dblclick', () => {
-              for (const $image of images) {
-                $image.hide();
-              }
-              $baseBranchMark.hide();
-              $headBranchMark.hide();
-              $diff.show();
-            });
-          }
+          $visualizedDiff.on('dblclick', () => {
+            $visualizedDiff.hide();
+            $diff.show();
+          });
           $diff.dblclick();
         }
       })
