eb9e7854d4ae600a960bd83868f8c5040f51d799
Sakamoto, Kazunori <exkazuu@gmail.com>
2020-04-18 20:51:28 +0900

    fix: update rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import closureCompile from 'rollup-plugin-closure-compile';
 import typescript from 'rollup-plugin-typescript2';
 
