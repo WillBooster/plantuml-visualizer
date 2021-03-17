1b6822137ef6778bc2fe98213a993ec603244bcb
kumachan-mis <y.s11nov@fuji.waseda.jp>
2021-03-17 15:27:37 +0900

    fix

--- src/popup.ts
+++ src/popup.ts
@@ -5,13 +5,13 @@ import { Constants } from './constants';
 function initPopup(): void {
   const $root = $(document.body).find('div#popup').css('width', '300px').css('height', '100px');
 
-  const toggleButton = $('<button>').attr('data-puml-vis-toggle', '').text('disable plantuml-visualizer');
+  const toggleButton = $('<button>').attr('data-puml-vis-toggle', '');
   toggleButton.on('click', () => {
     chrome.runtime.sendMessage({ command: Constants.commands.toggleExtensionEnabled }, onUpdateExtensionEnabled);
   });
   $root.append(toggleButton);
 
-  $root.append($('<p>').text(Constants.defaultImgSrcUrl));
+  $root.append($('<p>'));
   $root.append($('<input>').css('width', '280px'));
 
   const updateServerButton = $('<button>').attr('data-puml-vis-update-server', '').text('update server');
