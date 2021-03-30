93b21c70ddfd9edf41a5b5c7fc4929afc2db9715
Sakamoto, Kazunori <exkazuu@gmail.com>
2021-03-30 22:45:54 +0900

    build: employ yarn v2 without PnP

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,12 @@
-const micromatch = require('micromatch');
 const path = require('path');
+const micromatch = require('micromatch');
 
 module.exports = {
-  './{packages/*/,}{src,__tests__}/**/*.{js,jsx,ts,tsx}': ['eslint --fix'],
-  './**/*.{css,htm,html,js,json,jsx,md,scss,ts,tsx,vue,yaml,yml}': (files) => {
-    files = micromatch.not(files, './{packages/*/,}{src,__tests__}/**/*.{js,jsx,ts,tsx}');
+  './{src,__tests__}/**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  './**/*.{css,htm,html,js,json,json5,jsx,md,scss,ts,tsx,vue,yaml,yml}': (files) => {
+    files = micromatch.not(files, './{src,__tests__}/**/*.{js,jsx,ts,tsx}');
     const filteredFiles = files
-      .filter((file) => !file.includes('/test-fixtures/'))
+      .filter((file) => !file.includes('/test-fixtures/') && !file.includes('/packages/'))
       .map((file) => path.relative('', file));
     if (filteredFiles.length === 0) return [];
     const commands = [`prettier --write ${filteredFiles.join(' ')}`];
