3f86192f939f242944a18fb58d323a915af5acea
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2023-03-04 16:16:42 +0900

    install playwright

---
+++ __tests__/playwrightFixtures.ts
@@ -0,0 +1,25 @@
+import path from 'node:path';
+
+import { test as base, chromium, type BrowserContext } from '@playwright/test';
+
+export const test = base.extend<{ context: BrowserContext; extensionId: string }>({
+  // eslint-disable-next-line no-empty-pattern
+  context: async ({}, use) => {
+    // eslint-disable-next-line unicorn/prefer-module
+    const pathToExtension = path.join(__dirname, '..', 'dist');
+    const context = await chromium.launchPersistentContext('', {
+      headless: false,
+      args: [`--disable-extensions-except=${pathToExtension}`, `--load-extension=${pathToExtension}`],
+    });
+    await use(context);
+    await context.close();
+  },
+  extensionId: async ({ context }, use) => {
+    let [background] = context.serviceWorkers();
+    if (!background) background = await context.waitForEvent('serviceworker');
+    const extensionId = background.url().split('/')[2];
+    await use(extensionId);
+  },
+});
+
+export const expect = test.expect;
